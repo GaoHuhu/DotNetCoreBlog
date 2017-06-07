31c76726f191873f61795ce7b0938a62c49906ff
gaohuhu <778158781@qq.com>
2017-06-07 15:34:26 +0800

    test

--- Jack.Gao.Blog.DataAccess/DataAccessBase.cs
+++ Jack.Gao.Blog.DataAccess/DataAccessBase.cs
@@ -8,7 +8,7 @@ namespace Jack.Gao.Blog.DataAccess
 {
     public class DataAccessBase
     {
-        public static readonly string CONNECTION_STRING = @"Server=tcp:myblogdatabaseserver.database.windows.net,1433;Initial Catalog=MyBlog;
+        public static readonly string CONNECTION_STRING = @"Server=***;Initial Catalog=***;
                                                 Persist Security Info=False;User ID=***;Password=***;MultipleActiveResultSets=False;
                                                 Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
