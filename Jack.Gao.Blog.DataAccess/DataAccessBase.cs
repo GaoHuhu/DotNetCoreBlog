48212bcaec4f3bc450c901b641834581f7235cc0
gaohuhu <778158781@qq.com>
2017-06-07 15:31:52 +0800

    test

--- Jack.Gao.Blog.DataAccess/DataAccessBase.cs
+++ Jack.Gao.Blog.DataAccess/DataAccessBase.cs
@@ -9,7 +9,7 @@ namespace Jack.Gao.Blog.DataAccess
     public class DataAccessBase
     {
         public static readonly string CONNECTION_STRING = @"Server=tcp:myblogdatabaseserver.database.windows.net,1433;Initial Catalog=MyBlog;
-                                                Persist Security Info=False;User ID=usersql;Password=Jack_12345678;MultipleActiveResultSets=False;
+                                                Persist Security Info=False;User ID=***;Password=***;MultipleActiveResultSets=False;
                                                 Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
         public static bool ExecuteNonQuery(string sql, string connectionString, SqlParameter[] parameters)
